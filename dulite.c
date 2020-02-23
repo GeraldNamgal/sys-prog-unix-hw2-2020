a2af3fcdf2f6b57390f931b42d6d67d2d26c1e4f
mappia <gerald.arocena@gmail.com>
2020-02-23 04:08:51 -0500

    Valgrind passed

--- dulite.c
+++ dulite.c
@@ -4,15 +4,15 @@
  *	note:     
  */
 #include	<stdio.h>
+#include    <stdlib.h>
 #include	<dirent.h>
 #include	<sys/stat.h>
 #include	<string.h>
 
 #define	DATE_FMT	"%b %e %H:%M"		/* text format	*/
 
-static struct stat info;
-
 int disk_usage(char[]);
+void showInfo( char *, int );
 
 int main(int ac, char *av[])
 {
@@ -27,12 +27,15 @@ int main(int ac, char *av[])
 }
 
 /* disk_usage( char dirname[] )
- *	
+ * args: 
+ * rets:
+ * note: referenced
+ *       https://stackoverflow.com/questions/276827/string-padding-in-c	
  */
-int disk_usage( char dirname[] )
-{
-	DIR		*dir_ptr;		                /* the directory */
-	struct dirent	*direntp;		        /* each entry	 */
+int disk_usage( char dirname[] ) {
+	DIR	*dir_ptr;		                    /* the directory */
+	struct dirent *direntp;		            /* each entry	 */
+    struct stat info;
     int sumBlocks = 0;
     if ( lstat( dirname, &info) == -1 )		/* cannot stat	 */
 		perror( dirname );			        /* say why	     */    
@@ -43,17 +46,29 @@ int disk_usage( char dirname[] )
             sumBlocks += info.st_blocks;               // add directory's blocks
             while ( ( direntp = readdir( dir_ptr ) ) != NULL )   
                 if ( direntp->d_name[0] != '.' ) {        // skip '.' file names
-                    char path[1024];
+                    char *path;
+                    path = malloc( ( strlen(dirname) + strlen(direntp->d_name)
+                                        + 2 ) * sizeof( char ) );
                     strcat( strcpy( path, dirname ), "/" );  // concat base path
                     strcat( path, direntp->d_name );  // add file/directory name           
                     sumBlocks += disk_usage(path);  // add sub files/dirs blocks                  
+                    free( path );
                 }            
             closedir(dir_ptr);            
         }
     }
-    else                                    /* if file */
+    else                                    /* else a file */
         sumBlocks = info.st_blocks;
-    printf( "%d ", sumBlocks );
-	printf( "%s\n"  , dirname );
+    showInfo( dirname, sumBlocks );
     return sumBlocks;
+}
+
+/* showInfo( char *filename, int sumBlocks )
+ * args: 
+ * rets:
+ */
+void showInfo( char *filename, int sumBlocks )
+{
+	printf( "%-7d ", sumBlocks );
+	printf( "%s\n", filename);
 }
