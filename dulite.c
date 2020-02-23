276f539f4a88375c64d48fb402f6f2c42e9f1498
mappia <gerald.arocena@gmail.com>
2020-02-23 06:51:20 -0500

    bed

--- dulite.c
+++ dulite.c
@@ -16,40 +16,39 @@ void showInfo( char *, int );
 
 int main(int ac, char *av[])
 {
-	if ( ac == 1 ) {        
+	if ( ac == 1 )  
 		disk_usage( "." );
-    }
 	else
-		while ( --ac ){           
-			printf("%s:\n", *++av );
-			disk_usage( *av );
-		}
+		while ( --ac ) {
+            // TODO: check that [1] or [1] and [2] are -a and/or -k           
+			disk_usage( *++av );
+        }
 }
 
-/* disk_usage( char dirname[] )
+/* disk_usage( char pathname[] )
  * args: 
  * rets:
  * note: referenced
  *       https://stackoverflow.com/questions/276827/string-padding-in-c	
  */
-int disk_usage( char dirname[] ) {
+int disk_usage( char pathname[] ) {
 	DIR	*dir_ptr;		                    /* the directory */
 	struct dirent *direntp;		            /* each entry	 */
     struct stat info;
     int sumBlocks = 0;
-    if ( lstat( dirname, &info) == -1 )		/* cannot stat	 */
-		perror( dirname );			        /* say why	     */    
+    if ( lstat( pathname, &info) == -1 )	/* cannot stat	 */
+		perror( pathname );			        /* say why	     */    
     if ( S_ISDIR ( info.st_mode ) ) {       /* if directory  */
-        if ( ( dir_ptr = opendir( dirname ) ) == NULL )
-            fprintf(stderr,"dulite.c error: cannot open %s\n", dirname);
+        if ( ( dir_ptr = opendir( pathname ) ) == NULL )
+            fprintf(stderr,"dulite: cannot access '%s'\n", pathname);
         else {
             sumBlocks += info.st_blocks;               // add directory's blocks
             while ( ( direntp = readdir( dir_ptr ) ) != NULL )   
-                if ( direntp->d_name[0] != '.' ) {        // skip '.' file names
+                if ( direntp->d_name[0] != '.' ) {   // skip '.' beginning paths
                     char *path;
-                    path = malloc( ( strlen(dirname) + strlen(direntp->d_name)
+                    path = malloc( ( strlen(pathname) + strlen(direntp->d_name)
                                         + 2 ) * sizeof( char ) );
-                    strcat( strcpy( path, dirname ), "/" );  // concat base path
+                    strcat( strcpy( path, pathname ), "/" ); // concat base path
                     strcat( path, direntp->d_name );  // add file/directory name           
                     sumBlocks += disk_usage(path);  // add sub files/dirs blocks                  
                     free( path );
@@ -59,16 +58,16 @@ int disk_usage( char dirname[] ) {
     }
     else                                    /* else a file */
         sumBlocks = info.st_blocks;
-    showInfo( dirname, sumBlocks );
+    showInfo( pathname, sumBlocks );
     return sumBlocks;
 }
 
-/* showInfo( char *filename, int sumBlocks )
+/* showInfo( char *pathname, int sumBlocks )
  * args: 
  * rets:
  */
-void showInfo( char *filename, int sumBlocks )
+void showInfo( char *pathname, int sumBlocks )
 {
 	printf( "%-7d ", sumBlocks );
-	printf( "%s\n", filename);
+	printf( "%s\n", pathname);
 }
