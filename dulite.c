c2d95621694c96750b1c32c4a3a8376a2629d060
mappia <gerald.arocena@gmail.com>
2020-02-23 23:48:37 -0500

    New arg handling works

--- dulite.c
+++ dulite.c
@@ -5,6 +5,7 @@
  */
 #include	<stdio.h>
 #include    <stdlib.h>
+#include    <stdbool.h>
 #include	<dirent.h>
 #include	<sys/stat.h>
 #include	<string.h>
@@ -16,40 +17,38 @@ static bool kFlag = false;
 
 static int disk_usage(char[]);
 static void showInfo( char *, int );
-static void setOption(char*);
+static bool setOption(char*);
 
 /* main(int ac, char *av[])
  * 
  */
 // TODO: test arguments more (think they work though)
 int main(int ac, char *av[]) {
-	if ( ac == 1 )                          // if only one argument
+	if ( ac == 1 )                           // only one argument
 		disk_usage( "." );   
-    else if ( ac >= 3 ) {                   // if three or more arguments
-        if ( strcmp( av[1], "-a" ) == 0 || strcmp( av[1], "-k" ) == 0 ) {
-            if ( strcmp( av[2], "-a" ) == 0 || strcmp( av[2], "-k" ) == 0 ) {              
-                  setOption( av[1] ); setOption( av[2] ); 
-                  if ( ac == 3 ) {          // if only three args total
-                      disk_usage( "." );        
-                      ac = 1;               // advance counter (no files given)
-                  }
-                  else
-                      {ac -= 2; av += 2;}   // advance pointer past option args                
+    else if ( ac >= 3 ) {                    // three or more arguments
+        if ( setOption( av[1] ) && setOption( av[2] ) ) {  // two valid options?       
+            if ( ac == 3 ) {                 // only three args total
+                disk_usage( "." );        
+                ac = 1;                      // advance counter (no files given)
             }
-            else {                          // if only first arg is option
-                setOption( av[1] );
-                ac -= 1; av += 1;           // advance pointer past option arg
+            else {                           // more than three args
+                ac -= 2;                     // advance counter
+                av += 2;                     // advance pointer past option args                                
             }
+        }
+        else if ( setOption( av[1] ) ) {     // only first arg is valid option
+            ac -= 1;                         // advance counter
+            av += 1;                         // advance pointer past option arg        
         }
     }
-    else {                                  // if there are only two arguments
-        if ( strcmp( av[1], "-a" ) == 0 || strcmp( av[1], "-k" ) == 0 ) {
-            setOption( av[1] );             
+    else {                                   // only two arguments
+        if ( setOption( av[1] ) ) {          // if valid option given                   
             disk_usage( "." );
-            ac = 1;                         // advance counter (no files given)
+            ac = 1;                          // advance counter (no files given)
         }
     }	    
-    while ( --ac )                          // if filename(s) given
+    while ( --ac )                           // if filename(s) given
         disk_usage( *++av ); 
 }
 
@@ -57,15 +56,35 @@ int main(int ac, char *av[]) {
  *
  */
 // TODO 
-static void setOption(char* option)
-{
+static bool setOption(char* option) {
     if ( strcmp( "-a", option ) == 0 ) {
         aFlag = true;
+        return true;
     }
-
     if ( strcmp( "-k", option ) == 0 ) {
         kFlag = true;
+        return true;
     }
+    if ( '-' == option[0] && strlen(option) > 1 ) {
+        bool aFlagFound = false, kFlagFound = false;
+        for (size_t i = 1; i < strlen(option); i++) {
+            if ( option[i] != 'a' && option[i] != 'k') {
+                printf("dulite: invalid option -- '%c'\n", option[i]);
+                exit(1);
+            }
+            else if ( aFlagFound == false && option[i] == 'a') {
+                aFlag = true;
+                aFlagFound = true;
+            }
+            else if ( kFlagFound == false && option[i] == 'k') {
+                kFlag = true;
+                kFlagFound = true;
+            }           
+        }        
+        if (aFlagFound || kFlagFound)
+            return true;
+    }
+    return false;
 }
 
 /* disk_usage( char pathname[] )
