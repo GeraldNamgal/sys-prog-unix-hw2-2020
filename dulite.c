469596f4dadc27d60ca72ace758b8ea9e913d9db
mappia <gerald.arocena@gmail.com>
2020-02-23 22:20:30 -0500

    Arg handling works. Going to change strategy for more accuracy

--- dulite.c
+++ dulite.c
@@ -11,18 +11,61 @@
 
 #define	DATE_FMT	"%b %e %H:%M"		/* text format	*/
 
-int disk_usage(char[]);
-void showInfo( char *, int );
+static bool aFlag = false;
+static bool kFlag = false;
 
-int main(int ac, char *av[])
-{
-	if ( ac == 1 )  
-		disk_usage( "." );
-	else
-		while ( --ac ) {
-            // TODO: check that [1] or [1] and [2] are -a and/or -k           
-			disk_usage( *++av );
+static int disk_usage(char[]);
+static void showInfo( char *, int );
+static void setOption(char*);
+
+/* main(int ac, char *av[])
+ * 
+ */
+// TODO: test arguments more (think they work though)
+int main(int ac, char *av[]) {
+	if ( ac == 1 )                          // if only one argument
+		disk_usage( "." );   
+    else if ( ac >= 3 ) {                   // if three or more arguments
+        if ( strcmp( av[1], "-a" ) == 0 || strcmp( av[1], "-k" ) == 0 ) {
+            if ( strcmp( av[2], "-a" ) == 0 || strcmp( av[2], "-k" ) == 0 ) {              
+                  setOption( av[1] ); setOption( av[2] ); 
+                  if ( ac == 3 ) {          // if only three args total
+                      disk_usage( "." );        
+                      ac = 1;               // advance counter (no files given)
+                  }
+                  else
+                      {ac -= 2; av += 2;}   // advance pointer past option args                
+            }
+            else {                          // if only first arg is option
+                setOption( av[1] );
+                ac -= 1; av += 1;           // advance pointer past option arg
+            }
         }
+    }
+    else {                                  // if there are only two arguments
+        if ( strcmp( av[1], "-a" ) == 0 || strcmp( av[1], "-k" ) == 0 ) {
+            setOption( av[1] );             
+            disk_usage( "." );
+            ac = 1;                         // advance counter (no files given)
+        }
+    }	    
+    while ( --ac )                          // if filename(s) given
+        disk_usage( *++av ); 
+}
+
+/* setOption(char*)
+ *
+ */
+// TODO 
+static void setOption(char* option)
+{
+    if ( strcmp( "-a", option ) == 0 ) {
+        aFlag = true;
+    }
+
+    if ( strcmp( "-k", option ) == 0 ) {
+        kFlag = true;
+    }
 }
 
 /* disk_usage( char pathname[] )
@@ -31,7 +74,7 @@ int main(int ac, char *av[])
  * note: referenced
  *       https://stackoverflow.com/questions/276827/string-padding-in-c	
  */
-int disk_usage( char pathname[] ) {
+static int disk_usage( char pathname[] ) {
 	DIR	*dir_ptr;		                    /* the directory */
 	struct dirent *direntp;		            /* each entry	 */
     struct stat info;
@@ -66,7 +109,8 @@ int disk_usage( char pathname[] ) {
  * args: 
  * rets:
  */
-void showInfo( char *pathname, int sumBlocks )
+// TODO
+static void showInfo( char *pathname, int sumBlocks )
 {
 	printf( "%-7d ", sumBlocks );
 	printf( "%s\n", pathname);
